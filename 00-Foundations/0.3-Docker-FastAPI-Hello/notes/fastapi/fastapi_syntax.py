0a8e372b294041e1f168fd38ad871d2caad527ef
nishant4671 <nishantkr4671@gmail.com>
2026-05-26 21:04:26 +0530

    26 may

---
+++ 00-Foundations/0.3-Docker-FastAPI-Hello/notes/fastapi/fastapi_syntax.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello, World!"}
+
